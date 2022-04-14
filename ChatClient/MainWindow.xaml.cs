e2ada5e63e29d2010c9aaf0054704047ffc3cba2
Redice1997 <roshin-vladimir@mail.ru>
2022-04-15 02:49:42 +0300

    Завершение логики работы клиента

--- ChatClient/MainWindow.xaml.cs
+++ ChatClient/MainWindow.xaml.cs
@@ -12,23 +12,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChatClient.ServiceChat;
 
 namespace ChatClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window , IServiceChatCallback
     {
         bool isConnected = false;
+        ServiceChatClient client;
+        int ID;
         public MainWindow()
         {
             InitializeComponent();
+        }        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
         }
+
         void ConnectUser()
         {
             if (!isConnected)
             {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
                 tbUserName.IsEnabled = false;
                 bConDiscon.Content = "Disconnect";
                 isConnected = true;
@@ -38,6 +48,8 @@ namespace ChatClient
         {
             if (!isConnected)
             {
+                client.Disconnect(ID);
+                client = null;
                 tbUserName.IsEnabled = true;
                 bConDiscon.Content = "Connect";
                 isConnected = false;
@@ -52,6 +64,25 @@ namespace ChatClient
             else
             {
                 ConnectUser();
+            }
+        }
+
+        public void MessageCallback(string message)
+        {
+            lbChat.Items.Add(message);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                client?.SendMessage(tbMessage.Text, ID);
+                tbMessage.Text = string.Empty;
             }
         }
     }
